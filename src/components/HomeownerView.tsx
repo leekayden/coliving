9be08cc7bfda28a805403e49eb504ad0d26782cc
leekayden <kayden@programmer.net>
2023-02-16 22:01:18 +0800

    Update HomeownerView.tsx

--- src/components/HomeownerView.tsx
+++ src/components/HomeownerView.tsx
@@ -3,9 +3,10 @@ import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import CreateProperty from "./CreateProperty";
 import HomeownerCard from "./HomeownerCard";
-import { PropertyList } from "../global/data";
+import { PropertyList, isHomeownerAccount } from "../global/data";
+import { AppName } from "../global/definitions";
 
-function HomeownerView() {
+function Homeowner() {
   let ownerId = 0;
   const filteredList = PropertyList.filter((item) => item.ownerId === ownerId);
   return (
@@ -20,6 +21,22 @@ function HomeownerView() {
       ))}
     </div>
   );
+}
+
+function Guest() {
+  return(<div><NavBar isHomeOwner={false} /></div>);
+}
+
+interface HomeownerViewProps {
+  isHomeownerAccountArg?: boolean;
+}
+
+function HomeownerView({isHomeownerAccountArg}: HomeownerViewProps) {
+  if (isHomeownerAccount || isHomeownerAccountArg === true) {
+    return <Homeowner />;
+  } else {
+    return <Guest />;
+  }
 }
 
 export default HomeownerView;
