46e5ec7eb4c19997bcee2529107e24fb591ecd7d
leekayden <kayden@programmer.net>
2023-02-27 21:47:09 +0800

    update

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -117,7 +117,12 @@ export default function Gallery({ data, showNav }: GalleryProps) {
                 sx={{ width: 300 }}
                 getOptionLabel={(option) => option.title}
                 renderInput={(params) => (
-                  <TextField {...params} label="Search Properties" variant="filled" helperText="Search By Property Name" />
+                  <TextField
+                    {...params}
+                    label="Search Properties"
+                    variant="filled"
+                    helperText="Search By Property Name"
+                  />
                 )}
               />
             </Stack>
