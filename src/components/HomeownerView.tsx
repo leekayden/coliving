74b7694dc79b355104fd3831307b752a9473a872
Kayden Lee <kayden@programmer.net>
2023-02-28 15:35:31 +0800

    update

--- src/components/HomeownerView.tsx
+++ src/components/HomeownerView.tsx
@@ -9,7 +9,6 @@ import { forbiddenErrorMsg } from "../global/definitions";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import Grid from "@mui/material/Grid";
