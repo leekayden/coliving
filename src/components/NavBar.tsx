a3afe1edd88d6ede5ccdd799b3ed398c9c529840
Kayden Lee <kayden@programmer.net>
2023-02-28 16:03:38 +0800

    update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -248,7 +248,7 @@ export default function NavBar({ appname, isHomeOwner, noNav }: NavBarProps) {
                   variant="contained"
                   onClick={handleLink(link.path.toLowerCase())}
                   disableElevation
-                  disabled
+                  disabled={link.disabled}
                 >
                   {link.title ? link.title : link.path}
                 </Button>
