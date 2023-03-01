c8916322a110e2300630692b95c1822384b7cb7b
Kayden Lee <kayden@programmer.net>
2023-03-01 16:47:51 +0800

    update

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -12,9 +12,9 @@ import FeaturedPost from './FeaturedPost';
 import Main from './Main';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
-import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
+// import post1 from './blog-post.1.md';
+// import post2 from './blog-post.2.md';
+// import post3 from './blog-post.3.md';
 
 const sections = [
   { title: 'Technology', url: '#' },
@@ -57,7 +57,8 @@ const featuredPosts = [
   },
 ];
 
-const posts = [post1, post2, post3];
+// const posts = [post1, post2, post3];
+const posts = [1, 2, 3];
 
 const sidebar = {
   title: 'About',
