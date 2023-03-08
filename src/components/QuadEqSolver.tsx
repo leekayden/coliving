6647794d825b1c90705f804869e36e8eefa26a93
Kayden Lee <kayden@programmer.net>
2023-03-08 16:39:25 +0800

    update

--- src/components/QuadEqSolver.tsx
+++ src/components/QuadEqSolver.tsx
@@ -1,56 +1,32 @@
 import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
+import TextField from '@mui/material/TextField';
 
 function QuadEqSolver() {
-  const [a, setA] = useState<number | "">("");
-  const [b, setB] = useState<number | "">("");
-  const [c, setC] = useState<number | "">("");
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+  const [c, setC] = useState("");
   const [result, setResult] = useState("");
 
   const solveEquation = () => {
-    const discriminant = Number(b) * Number(b) - 4 * Number(a) * Number(c);
+    const discriminant = +b * +b - 4 * +a * +c;
     if (discriminant < 0) {
       setResult("No real solutions.");
     } else if (discriminant === 0) {
-      const x = -Number(b) / (2 * Number(a));
-      const equation = `${Number(a)}x^2${Number(b) < 0 ? "" : "+"}${Number(
-        b
-      )}x${Number(c) < 0 ? "" : "+"}${Number(c)}`;
-      setResult(`x = (${x} + ${x}) = ${equation.replace("x", `(${x}+${x})`)}`);
+      const x = (-b + Math.sqrt(+discriminant)) / (2 * +a);
+      setResult(`x = ${x}`);
     } else {
-      const x1 = (-Number(b) + Math.sqrt(discriminant)) / (2 * Number(a));
-      const x2 = (-Number(b) - Math.sqrt(discriminant)) / (2 * Number(a));
-      const equation = `${Number(a)}x^2${Number(b) < 0 ? "" : "+"}${Number(
-        b
-      )}x${Number(c) < 0 ? "" : "+"}${Number(c)}`;
-      setResult(`x1 = ${x1}, x2 = ${x2} => ${equation}`);
+      const x1 = (-b + Math.sqrt(+discriminant)) / (2 * +a);
+      const x2 = (-b - Math.sqrt(+discriminant)) / (2 * +a);
+      setResult(`x1 = ${x1}, x2 = ${x2}`);
     }
   };
 
   return (
     <div>
       <p>Enter in the form of ax^2 + bx + c</p>
-      <TextField
-        id="outlined-basic-a"
-        label="a"
-        variant="outlined"
-        value={a}
-        onChange={(e) => setA(e.target.value)}
-      />
-      <TextField
-        id="outlined-basic-b"
-        label="b"
-        variant="outlined"
-        value={b}
-        onChange={(e) => setB(e.target.value)}
-      />
-      <TextField
-        id="outlined-basic-c"
-        label="c"
-        variant="outlined"
-        value={c}
-        onChange={(e) => setC(e.target.value)}
-      />
+      <TextField id="outlined-basic-a" label="a" variant="outlined" value={a} onChange={(e) => setA(e.target.value)} />
+      <TextField id="outlined-basic-b" label="b" variant="outlined" value={b} onChange={(e) => setB(e.target.value)} />
+      <TextField id="outlined-basic-c" label="c" variant="outlined" value={c} onChange={(e) => setC(e.target.value)} />
       <button onClick={solveEquation}>Solve</button>
       <p>{result}</p>
     </div>
