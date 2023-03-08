5b503ce70fda4291f353d8b828a4b92cd107d06e
Kayden Lee <kayden@programmer.net>
2023-03-08 17:09:23 +0800

    update

--- src/components/QuadEqSolver.tsx
+++ src/components/QuadEqSolver.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 
 function QuadEqSolver() {
   const [a, setA] = useState("");
@@ -17,17 +17,39 @@ function QuadEqSolver() {
     } else {
       const x1 = (-b + Math.sqrt(+discriminant)) / (2 * +a);
       const x2 = (-b - Math.sqrt(+discriminant)) / (2 * +a);
-      setResult(`(x - ${Math.round(x1)})(x - ${Math.round(x2)})`);
+      if (x2 === 0) {
+        setResult(`(x - ${x1})x`);
+      } else {
+        setResult(`(x - ${Math.round(x1)})(x - ${Math.round(x2)})`);
+      }
     }
-  };  
+  };
 
   return (
     <div>
       <p>Enter in the form of ax^2 + bx + c</p>
-      <br/>
-      <TextField id="outlined-basic-a" label="a" variant="outlined" value={a} onChange={(e) => setA(e.target.value)} />
-      <TextField id="outlined-basic-b" label="b" variant="outlined" value={b} onChange={(e) => setB(e.target.value)} />
-      <TextField id="outlined-basic-c" label="c" variant="outlined" value={c} onChange={(e) => setC(e.target.value)} />
+      <br />
+      <TextField
+        id="outlined-basic-a"
+        label="a"
+        variant="outlined"
+        value={a}
+        onChange={(e) => setA(e.target.value)}
+      />
+      <TextField
+        id="outlined-basic-b"
+        label="b"
+        variant="outlined"
+        value={b}
+        onChange={(e) => setB(e.target.value)}
+      />
+      <TextField
+        id="outlined-basic-c"
+        label="c"
+        variant="outlined"
+        value={c}
+        onChange={(e) => setC(e.target.value)}
+      />
       <button onClick={solveEquation}>Solve</button>
       <p>{result}</p>
     </div>
