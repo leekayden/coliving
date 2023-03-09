1e70465546c306e7c051ca2a4e78dc774ca08910
Kayden Lee <kayden@programmer.net>
2023-03-09 20:53:29 +0800

    update

--- src/components/OddEvenChecker.tsx
+++ src/components/OddEvenChecker.tsx
@@ -8,6 +8,8 @@ function OddEvenChecker() {
       alert("Please enter a number.");
     } else if (isNaN(+number)) {
       alert("Please enter a valid number.");
+    } else if (number === 0) {
+      alert("The number is even. P.S. the identity of 0 is under debate, but for now, it's even.")
     } else {
       const result = +number % 2 === 0 ? "even" : "odd";
       alert(`The number is ${result}.`);
