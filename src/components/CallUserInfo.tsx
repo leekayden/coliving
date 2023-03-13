7d0164aaf71ebdfa2ae041438f3609978446f400
Kayden Lee <kayden@programmer.net>
2023-03-13 16:45:33 +0800

    update

--- src/components/CallUserInfo.tsx
+++ src/components/CallUserInfo.tsx
@@ -8,17 +8,40 @@ const CallUserInfo: React.FC = () => {
   const [screenHeight, setScreenHeight] = React.useState<number>(0);
 
   React.useEffect(() => {
-    // Get user agent string
+    // Get the user agent string
     setUserAgent(window.navigator.userAgent);
 
-    // Get IP address
+    // Get the IP address (using a free API)
     fetch("https://api.ipify.org/?format=json")
       .then((response) => response.json())
       .then((data) => setIpAddress(data.ip));
 
-    // Get screen size
+    // Get the screen size
     setScreenWidth(window.screen.width);
     setScreenHeight(window.screen.height);
+
+    // Get the user's location
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+
+        try {
+          const response = await fetch(
+            `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=66547c6b30a9456a92f26d356479fd3d`
+          );
+
+          const data = await response.json();
+
+          const city = data.results[0]?.components.city || null;
+          const country = data.results[0]?.components.country || null;
+
+          setCity(city);
+          setCountry(country);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+    }
   }, []);
 
   return (
