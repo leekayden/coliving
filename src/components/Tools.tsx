572714a629e244a8649d0042ed25c22873ca1626
Kayden Lee <kayden@programmer.net>
2023-03-13 13:36:37 +0800

    update

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -40,7 +40,7 @@ export default function Tools() {
                     flexDirection: "column",
                   }}
                 >
-                  <CardActionArea style={{height: "100%"}}>
+                  <CardActionArea style={{ height: "100%" }}>
                     <CardMedia
                       component="img"
                       height="140"
