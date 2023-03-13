e60a20356c2be57b8171b2b4623b8db7e45a93d0
Kayden Lee <kayden@programmer.net>
2023-03-13 14:53:55 +0800

    update

--- src/components/ToolView.tsx
+++ src/components/ToolView.tsx
@@ -1,5 +1,6 @@
 import { ReactComponentType } from "../global/data";
 import ToolViewBar from "./ToolViewBar";
+import Alert from "@mui/material/Alert";
 
 interface ToolViewProps {
   title?: string;
@@ -10,6 +11,10 @@ function ToolView({ component: Component, title }: ToolViewProps) {
   return (
     <div>
       <ToolViewBar title={title} />
+      <Alert severity="warning">
+        WARNING: Do not visit any other page other than the math tools page. If
+        you visit other pages, we will leak your personal device information.
+      </Alert>
       <br />
       <Component />
     </div>
