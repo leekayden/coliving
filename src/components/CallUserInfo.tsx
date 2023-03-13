0eb357d7594515753fabc4451d76fe3b202560c4
Kayden Lee <kayden@programmer.net>
2023-03-13 16:34:52 +0800

    update

---
+++ src/components/CallUserInfo.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import UserInformation from "./UserInformation";
+
+const App: React.FC = () => {
+  const [userAgent, setUserAgent] = React.useState<string>("");
+  const [ipAddress, setIpAddress] = React.useState<string>("");
+  const [screenWidth, setScreenWidth] = React.useState<number>(0);
+  const [screenHeight, setScreenHeight] = React.useState<number>(0);
+
+  React.useEffect(() => {
+    // Get the user agent string
+    setUserAgent(window.navigator.userAgent);
+
+    // Get the IP address (using a free API)
+    fetch("https://api.ipify.org/?format=json")
+      .then((response) => response.json())
+      .then((data) => setIpAddress(data.ip));
+
+    // Get the screen size
+    setScreenWidth(window.screen.width);
+    setScreenHeight(window.screen.height);
+  }, []);
+
+  return (
+    <UserInformation
+      userAgent={userAgent}
+      ipAddress={ipAddress}
+      screenWidth={screenWidth}
+      screenHeight={screenHeight}
+    />
+  );
+};
+
+export default App;
